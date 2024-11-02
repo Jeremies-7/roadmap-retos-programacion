77190c66604cbbae6dd0a04ac3e69f624d771bef
Jeremies-7 <jereperezyaya21@gmail.com>
2024-11-01 22:11:21 -0300

    Jeremies-7.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jeremies-7.py
@@ -0,0 +1,34 @@
+# https://python.org/
+
+# Esto es un comentario de una linea
+
+” ” ”
+Esto es
+un comentario de 
+Varias lineas
+” ” ”
+
+'''
+Esto es
+un comentario de 
+Varias lineas
+'''
+my_variable = 'Mi variable'
+my_variable = 'Mi nueva variable'
+
+MY_CONSTANT = 'Mi constante' # por convención 
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = 'Mi cadena de texto'
+my_other_string = 'Mi otra cadena de texto'
+
+print('¡Hola, Python!')
+
+
+
+
+
+
